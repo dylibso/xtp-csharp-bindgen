2a37197df2d2255a572993a7db31fac4d68467ef
Muhammad Azeez <muhammad@dylibso.com>
2024-08-12 08:04:09 +0300

    fix slashes

--- tests/test.ps1
+++ tests/test.ps1
@@ -3,12 +3,12 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
 # Get all YAML files in the schemas directory
-$files = Get-ChildItem -Path .\tests\schemas\*.yaml
+$files = Get-ChildItem -Path ./tests/schemas/*.yaml
 
 foreach ($file in $files) {
     Write-Host "Generating and testing $($file.FullName)..."
 
-    $outputFolder = ".\tests\output\$($file.BaseName)"
+    $outputFolder = "./tests/output/$($file.BaseName)"
 
-    xtp plugin init --schema-file $file.FullName --template .\bundle --path $outputFolder -y --feature stub-with-code-samples --name output
+    xtp plugin init --schema-file $file.FullName --template ./bundle --path $outputFolder -y --feature stub-with-code-samples --name output
 }
